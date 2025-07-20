050188f1f9132a2e466a03cd261faf445e2e235f
Dorijan Di Zepp <dorijan.dizepp@gmail.com>
2025-07-20 16:56:21 +0200

    feat: update peer list on join procedure

--- app/src/main/java/mars/Main.java
+++ app/src/main/java/mars/Main.java
@@ -3,16 +3,25 @@ package mars;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
+import mars.Node.JoinMsg;
+
 import mars.Client.UpdateMsg;
 import mars.Client.GetMsg;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
 public class Main {
     
+    public static void delay(){
+        try{
+            Thread.sleep(rand.nextInt(300) + 200);
+        }catch(InterruptedException e){
+            System.err.println(e);
+        }
+    }
+
     //user.dir will make sure that all logs are generated inside the projet folder starting from app
     private static final String LOGGER_FILE_BASE_PATH = System.getProperty("user.dir") + "/logs";
     private static final int N_NODES = 2;
@@ -29,37 +38,34 @@ public class Main {
         Logger logger = new Logger(LOGGER_FILE_BASE_PATH + "/test.txt");
         List<ActorRef> nodeList = new ArrayList<ActorRef>();
 
-        // create some nodes and store them in the actor_ref list
-        //TODO: not the correct way to update the peer list of all nodes
-        //the correct procedure would be to make a join request to the network and after that
-        //make another request to update all lists of all nodes
-        for(int i=0; i<N_NODES; ++i){
-            Integer name = rand.nextInt(80 - 5) + 5;
-            ActorRef node = system.actorOf(Node.props(name, logger), name.toString());
-            nodeList.add(node);
-        }
+        // create some nodes and make them join the network through the correct message
+        //this will be used as the bootstrap for every new node
+        ActorRef bootstrap = system.actorOf(Node.props(40, logger), "40");
+        nodeList.add(bootstrap);
 
-        //order the list so that we already have a "ring" structure
-        //this list should be available to all nodes of the network
-        nodeList.sort(Comparator.comparingInt(ref -> {
-            String name = ref.path().name();//e.g., "10"
-            return Integer.parseInt(name);//parse "10"
-        }));
+        bootstrap.tell(new JoinMsg(bootstrap), null);
+
+        nodeList.add(system.actorOf(Node.props(20, logger), "20"));
+        nodeList.add(system.actorOf(Node.props(30, logger), "30"));
+        nodeList.add(system.actorOf(Node.props(10, logger), "10"));
+        
+        nodeList.get(1).tell(new JoinMsg(bootstrap), null);
+        //delay();
+        nodeList.get(2).tell(new JoinMsg(bootstrap), null);
+        //delay();
+        nodeList.get(3).tell(new JoinMsg(nodeList.get(2)), null);
+        //delay();
 
         // create some clients and ask them to make some requests
-        ActorRef client_1 = system.actorOf(Client.props("C1", logger, nodeList), "C1");
-        ActorRef client_2 = system.actorOf(Client.props("C2", logger, nodeList), "C2");
-        client_1.tell(new UpdateMsg(10, "IRON"), null);
-        client_1.tell(new GetMsg(10), null);
-        client_2.tell(new UpdateMsg(10, "COPPER"), null);
-        client_2.tell(new GetMsg(10), null);
+        // note that even if nodeList is not ordered like a ring, it doesn't
+        // matter as clients can ask anyone
+        // ActorRef client_1 = system.actorOf(Client.props("C1", logger, nodeList), "C1");
+        // ActorRef client_2 = system.actorOf(Client.props("C2", logger, nodeList), "C2");
+        // client_1.tell(new UpdateMsg(10, "IRON"), null);
+        // client_1.tell(new GetMsg(10), null);
+        // client_2.tell(new UpdateMsg(10, "COPPER"), null);
+        // client_2.tell(new GetMsg(10), null);
 
-        // System.out.println("\nRunning. Press Ctrl+C to terminate.");
-        // try {
-        //     Thread.sleep(Long.MAX_VALUE);
-        // } catch (InterruptedException e) {
-        //     System.out.println("Interrupted. Shutting down.");
-        // }
         system.terminate();
     }
 }
--- app/src/main/java/mars/Node.java
+++ app/src/main/java/mars/Node.java
@@ -8,7 +8,11 @@ import mars.Client.UpdateMsg;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,6 +21,7 @@ public class Node extends AbstractActor{
     enum State {
         JOIN,
         LEAVE,
+        STABLE,
         CRASH,
         RECOVERY,
     }
@@ -26,9 +31,9 @@ public class Node extends AbstractActor{
     private Map<Integer, VersionedValue> storage;
     public State state;//so that clients/nodes avoid to contact a crashed node and wait indefinetly
     private boolean isCoordinator;
-    private boolean isQuorumMet;//as timeout msg is scheduled, check flag to determine 
+    private boolean isQuorumReached;//as timeout msg is scheduled, check flag to determine 
                                 //if quorum has been reached before the timeout
-    private List<ActorRef> peerList;
+    private Set<ActorRef> peerList;//contains also yourself
     private int N = 4,//replication factor
                 R = 3,//reading quorum 
                 W = 3,//writing quorum
@@ -47,6 +52,22 @@ public class Node extends AbstractActor{
 
     // MESSAGES --------------------------------------------
     public static class TimeoutMsg implements Serializable {}
+
+    public static class RequestPeersMsg implements Serializable {}
+
+    public static class PeersMsg implements Serializable {
+        public final Set<ActorRef> peerList;
+        public PeersMsg(Set<ActorRef> peerList){
+            this.peerList = peerList;
+        }
+    }
+
+    public static class JoinMsg implements Serializable {
+        public final ActorRef bootstrap;
+        public JoinMsg(ActorRef bootstrap){
+            this.bootstrap = bootstrap;
+        }
+    }
 
     public static class UpdateResponse implements Serializable {
         public final boolean isValid;
@@ -70,12 +91,18 @@ public class Node extends AbstractActor{
     public Node(Integer name, Logger logger){
         this.name = name;
         this.logger = logger;
-        this.peerList = new ArrayList<>();
+        this.peerList = new TreeSet<ActorRef>((a1, a2) -> {
+            String name1 = a1.path().name();
+            String name2 = a2.path().name();
+            return Integer.compare(Integer.parseInt(name1), Integer.parseInt(name2));
+        });
+
+        this.peerList.add(getSelf());//add yourself to list
         
         this.storage = new HashMap<>();
         this.state = State.JOIN;
         this.isCoordinator = false;
-        this.isQuorumMet = false;
+        this.isQuorumReached = false;
 
     }
 
@@ -88,8 +115,41 @@ public class Node extends AbstractActor{
      * function called after class constructor
      * it allows to execute the join procedure in order
      */
-    private void onJoinMsg(){
-        //here it will update its peerList
+    private void onJoinMsg(JoinMsg msg){
+        //msg will contain the bootstrap node to ask for the peer list
+        ActorRef bootstrap = msg.bootstrap;
+        logger.log(this.name.toString(), "Requesting peers list from " + bootstrap.path().name());
+        bootstrap.tell(new RequestPeersMsg(), getSelf());
+    }
+
+    public void onRequestPeersMsg(RequestPeersMsg msg){
+        // the bootstrap simply send back its list of peers
+        getSender().tell(new PeersMsg(this.peerList), getSelf());
+    }
+
+    private void onPeersMsg(PeersMsg msg){
+        //the nodes update the peer list
+        this.peerList.addAll(msg.peerList);
+        logger.log(this.name.toString(), "Updated peer list with now " + this.peerList.size() + " nodes");
+
+        //TODO: retrieve the required data
+
+        //the joining node needs to announce itself to the network
+        //before sending the list of peers, update it by adding the itself
+        //and check if list is ordered so that we respect the "ring" structure>
+
+        //if it is a stable node, it doesn't need to update other nodes
+        if(this.state == State.STABLE) return;
+
+        //send new list to all other nodes exept yourself
+        for(ActorRef node : this.peerList){
+            if(node != getSelf()){
+                getSender().tell(new PeersMsg(this.peerList), getSelf());
+            }
+        }
+
+        this.state = State.STABLE;//now the node is stable in the network
+        logger.log(this.name.toString(), "Now STABLE in the network with " + this.peerList.size() + " nodes");
     }
 
     // BEHAVIOR -----------------------------------------
@@ -130,12 +190,16 @@ public class Node extends AbstractActor{
      */
     private void crash(){
         getContext().become(crashed());
+        this.state = State.CRASH;
     }
 
     //define the mapping between the received message types and actor methods
     @Override
     public Receive createReceive() {
       return receiveBuilder()
+        .match(JoinMsg.class, this::onJoinMsg)
+        .match(RequestPeersMsg.class, this::onRequestPeersMsg)
+        .match(PeersMsg.class, this::onPeersMsg)
         .match(UpdateMsg.class, this::onUpdateMsg)
         .match(GetMsg.class, this::onGetMsg)
         .matchAny(msg -> {
